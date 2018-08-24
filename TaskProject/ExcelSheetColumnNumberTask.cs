2001abe5eed42feb1a5839119478c1713b7fa5c1
Ken Chiu <ken.chiu@ehsn.com.tw>
2018-08-24 13:33:17 +0800

    Add ExcelSheetColnumnNumber Task

---
+++ TaskProject/ExcelSheetColumnNumberTask.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskProject
+{
+    public class ExcelSheetColumnNumberTask
+    {
+        /// <summary>
+        /// Given a column title as appear in an Excel sheet, return its corresponding column number.
+        ///  For example:  
+        ///     A -> 1   ,   B -> 2 ,    C -> 3
+        ///     Z -> 26 AA -> 27  AB -> 28 
+        ///     Case 1 Input "A" Output 1
+        ///     Case 2 Input "AB Output 28
+        ///     Case 3 Input "ZY" Output 701
+        /// </summary>
+        /// <param name="s">The s.</param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public int TitleToNumber(string s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
